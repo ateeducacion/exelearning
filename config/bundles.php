67bdcdc1b7f49c257ea4bc49c6c34697ad0bebc7
Ernesto Serrano <info@ernesto.es>
2025-08-19 15:20:21 +0100

    Add API Platform with example Book resource

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];

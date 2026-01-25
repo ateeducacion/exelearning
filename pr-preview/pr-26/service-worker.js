adbf4c1c6c332a822477c13dfab5b4fc4e2b222b
erseco <erseco@users.noreply.github.com>
2026-01-25 00:07:02 +0000

    Deploy preview for PR 26 🛫

--- pr-preview/pr-26/service-worker.js
+++ pr-preview/pr-26/service-worker.js
@@ -3,7 +3,7 @@
  * Provides offline-first caching for PWA
  */
 
-const CACHE_NAME = 'exelearning-static-v0.0.0-pr26-202601250004-e41e515';
+const CACHE_NAME = 'exelearning-static-v0.0.0-pr26-202601250006-95de76d';
 const STATIC_ASSETS = [
     './',
     './index.html',
